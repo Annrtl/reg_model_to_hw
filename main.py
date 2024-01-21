2b4dba571501b11c79244e4ee1684a5431405c64
AnnRTL <alexandre.thomas@gmx.com>
2024-01-21 18:52:48 +0100

    Improved model and add callback

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
+from tensorflow.keras import callbacks
+from tqdm import tqdm
 
 keras.utils.set_random_seed(55)
 
 def train_model(model, x_train, y_train, x_valid, y_valid):
+    early_stopping = callbacks.EarlyStopping(
+        min_delta=0.001,
+        patience=20,
+        restore_best_weights=True,
+    )
+
     history = model.fit(
         x_train, y_train,
         validation_data=(x_valid, y_valid),
         batch_size=512,
-        epochs=64,
+        epochs=1024,
+        callbacks=[early_stopping],
         verbose=1
     )
 
@@ -28,7 +37,7 @@ def train_model(model, x_train, y_train, x_valid, y_valid):
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Loss')
     plt.yscale('log')
-    # plt.show()
+    plt.show()
 
     model.save_weights("model/model.h5")
 
@@ -69,7 +78,10 @@ input_shape = [input_shape]
 print(f"Input shape is {input_shape}")
 
 model = keras.Sequential([
-    keras.layers.Dense(1, input_shape=input_shape),
+    keras.layers.Dense(128, input_shape=input_shape, activation="relu"),
+    keras.layers.Dense(64, activation="relu"),
+    keras.layers.Dense(32, activation="relu"),
+    keras.layers.Dense(16),
 ])
 
 model.compile(
@@ -77,20 +89,21 @@ model.compile(
     loss='mae',
 )
 
-if os.path.isfile("model/model.h5"):
+do_load = True
+
+if do_load and os.path.isfile("model/model.h5"):
     model.load_weights("model/model.h5")
 else:
     train_model(model, x_train, y_train, x_valid, y_valid)
 
-test = train_csv.sample(n=100)
-
 py_expected = []
 py_pred = []
-cpp_pred = []
+#cpp_pred = []
+err_array = []
 
-test = valid.sample(n=100)
+test = train_csv.sample(n=1000)
 
-for index, rows in test.iterrows():
+for index, rows in tqdm(test.iterrows()):
     row = rows.copy()
     row = row.to_frame()
     row = row.transpose()
@@ -101,23 +114,28 @@ for index, rows in test.iterrows():
     res = model.predict(row, verbose=0)
     res = res[0][0]
     py_pred.append(res)
+    err = abs(res - expected)
+    #err = res - expected
+    err_array.append(err)
     argv = row.values.tolist()[0]
     argv = list(map(str, argv))
     argv = " ".join(argv)
-    cpp_predict = os.popen(f"./build/nn {argv}").read()
-    cpp_pred.append(float(cpp_predict))
+    #cpp_predict = os.popen(f"./build/nn {argv}").read()
+    #cpp_pred.append(float(cpp_predict))
     #print(f"Predicted: {res}, cpp predicted: {cpp_predict} and Expected: {expected}")
 
-fig, ax = plt.subplots()
+fig, axs = plt.subplots(2, 1)
 x_ax = range(len(py_expected))
-ax.plot(x_ax, py_expected, label='Python DB (Expected value)')
-ax.plot(x_ax, py_pred, label='C++ Predicted value')
-y1 = list(map(lambda x: x*1.05, py_expected))
-y2 = list(map(lambda x: x*0.95, py_expected))
-#ax.fill_between(x_ax, y1, y2, alpha=.5, label='Python DB (Expected value +/- 5%)')
-ax.legend()
-ax.grid()
-ax.set_title('C++ Predicted values versus Python DataBase Expected values')
-ax.set_xlabel('Sample')
-ax.set_ylabel('Value')
+axs[0].plot(x_ax, py_expected, label='Python DB (Expected value)')
+axs[0].plot(x_ax, py_pred, label='C++ Predicted value')
+axs[0].legend()
+axs[0].grid()
+axs[0].set_title('C++ Predicted values versus Python DataBase Expected values')
+axs[0].set_xlabel('Sample')
+axs[0].set_ylabel('Value')
+axs[1].hist(err_array, bins=100)
+axs[1].grid()
+axs[1].set_title('Errors distribution')
+axs[1].set_xlabel('Error')
+axs[1].set_ylabel('Volume')
 plt.show()
